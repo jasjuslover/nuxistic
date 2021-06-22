e83ff6c60b5328c350e151da8eb01f5b6ed1c7d0
Husni Kamal <husnikamal3@gmail.com>
2021-06-22 21:40:43 +0700

    sb admin integration

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="wrapper">
+    <app-sidebar></app-sidebar>
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <app-header></app-header>
+        <nuxt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from "@/components/layout/Sidebar.vue";
+import Header from "@/components/layout/Header.vue";
+export default {
+  components: {
+    "app-sidebar": Sidebar,
+    "app-header": Header,
+  },
+};
+</script>
